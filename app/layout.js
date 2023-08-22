6696a696bec87554603b37e5e0c012a2c543cf52
chicken2609@hotmail.com <lesbianhaircut@outlook.com>
2023-08-22 00:27:04 -0400

    c

--- app/layout.js
+++ app/layout.js
@@ -4,12 +4,6 @@ import StyledComponentsRegistry from "@/lib/registry";
 import NextTopLoader from "nextjs-toploader";
 import Navbar from "@/components/Navbar";
 
-export const metadata = {
-  title: "Optimal Living Community",
-  description:
-    "Find your community and improve your health with the knowledge of the rwa primal diet. ",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
