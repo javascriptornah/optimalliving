65ac83829488f5e596f47a2ec31159a8aebe0694
chicken2609@hotmail.com <lesbianhaircut@outlook.com>
2023-08-30 18:32:36 -0400

    c

--- app/server/page.js
+++ app/server/page.js
@@ -1,11 +1,43 @@
 "use client";
 import { useState } from "react";
 import styled from "styled-components";
+import COLORS from "@/data/colors";
+import {
+  data_entries_us,
+  data_entries_us_two,
+  data_entries_canada,
+  data_entries_various,
+  stateMappings,
+} from "@/data/misc";
+import { pushUser, pushState, addUser } from "@/lib/SupabaseDevFunctions";
 
-const Cont = styled.div``;
+const Cont = styled.div`
+  min-height: 100vh;
+  padding-top: 160px;
+`;
 
 const Page = () => {
-  return <Cont colors={COLORS}></Cont>;
+  const func = () => {
+    const newArr = [];
+    for (let data of data_entries_various) {
+      newArr.push(data);
+    }
+    console.log("newArr");
+    console.log(newArr);
+  };
+
+  const func2 = async () => {
+    for (let user of data_entries_various) {
+      const res = await addUser(user);
+      console.log(res);
+    }
+  };
+  return (
+    <Cont colors={COLORS}>
+      <p onClick={}>Click me</p>
+      <p onClick={}>Click me two</p>
+    </Cont>
+  );
 };
 
 export default Page;
